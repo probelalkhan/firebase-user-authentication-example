51d311f6b4c26923a3499703def7220a18fa65bd
Belal Khan <Belal@MACs-MacBook-Air.local>
2016-07-31 14:34:51 +0530

    Initial commit

---
+++ app/src/main/java/net/simplifiedcoding/firebaseauthdemo/LoginActivity.java
@@ -0,0 +1,117 @@
+package net.simplifiedcoding.firebaseauthdemo;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    //defining views
+    private Button buttonSignIn;
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private TextView textViewSignup;
+
+    //firebase auth object
+    private FirebaseAuth firebaseAuth;
+
+    //progress dialog
+    private ProgressDialog progressDialog;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        //getting firebase auth object
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        //if the objects getcurrentuser method is not null
+        //means user is already logged in
+        if(firebaseAuth.getCurrentUser() != null){
+            //close this activity
+            finish();
+            //opening profile activity
+            startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
+        }
+
+        //initializing views
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        buttonSignIn = (Button) findViewById(R.id.buttonSignin);
+        textViewSignup  = (TextView) findViewById(R.id.textViewSignUp);
+
+        progressDialog = new ProgressDialog(this);
+
+        //attaching click listener
+        buttonSignIn.setOnClickListener(this);
+        textViewSignup.setOnClickListener(this);
+    }
+
+    //method for user login
+    private void userLogin(){
+        String email = editTextEmail.getText().toString().trim();
+        String password  = editTextPassword.getText().toString().trim();
+
+
+        //checking if email and passwords are empty
+        if(TextUtils.isEmpty(email)){
+            Toast.makeText(this,"Please enter email",Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if(TextUtils.isEmpty(password)){
+            Toast.makeText(this,"Please enter password",Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        //if the email and password are not empty
+        //displaying a progress dialog
+
+        progressDialog.setMessage("Registering Please Wait...");
+        progressDialog.show();
+
+        //logging in the user
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressDialog.dismiss();
+                        //if the task is successfull 
+                        if(task.isSuccessful()){
+                            //start the profile activity
+                            finish();
+                            startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
+                        }
+                    }
+                });
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == buttonSignIn){
+            userLogin();
+        }
+
+        if(view == textViewSignup){
+            finish();
+            startActivity(new Intent(this, MainActivity.class));
+        }
+    }
+}
---
+++ app/src/main/java/net/simplifiedcoding/firebaseauthdemo/ProfileActivity.java
@@ -0,0 +1,66 @@
+package net.simplifiedcoding.firebaseauthdemo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class ProfileActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //firebase auth object
+    private FirebaseAuth firebaseAuth;
+
+    //view objects
+    private TextView textViewUserEmail;
+    private Button buttonLogout;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        //initializing firebase authentication object
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        //if the user is not logged in
+        //that means current user will return null
+        if(firebaseAuth.getCurrentUser() == null){
+            //closing this activity
+            finish();
+            //starting login activity
+            startActivity(new Intent(this, LoginActivity.class));
+        }
+
+        //getting current user
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        //initializing views
+        textViewUserEmail = (TextView) findViewById(R.id.textViewUserEmail);
+        buttonLogout = (Button) findViewById(R.id.buttonLogout);
+
+        //displaying logged in user name
+        textViewUserEmail.setText("Welcome "+user.getEmail());
+
+        //adding listener to button
+        buttonLogout.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        //if logout is pressed
+        if(view == buttonLogout){
+            //logging out the user
+            firebaseAuth.signOut();
+            //closing activity
+            finish();
+            //starting login activity
+            startActivity(new Intent(this, LoginActivity.class));
+        }
+    }
+}
